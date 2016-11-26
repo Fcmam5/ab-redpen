3f59116b12b461effc90f6835efab7b0f147d5d1
fcmam5 <dr.fcmam5@gmail.com>
2016-11-26 22:42:42 +0100

    Redirect user from /board to /board/UNIQUE_URL

--- routes/board.js
+++ routes/board.js
@@ -3,8 +3,11 @@ var router = express.Router();
 
 /* GET the board page. */
 router.get('/', function(req, res, next) {
+    var x = new Date().valueOf();
+  res.redirect('board/'+x);
+});
+router.get('/:num', function(req, res, next) {
   res.render('board', { title: 'My board' });
 });
 
 module.exports = router;
-// TODO: create /board/UNIQUE_ID pages
