6614ce08c104cfcc73f9777c93cce312b9d8cc41
fcmam5 <dr.fcmam5@gmail.com>
2016-11-26 02:19:54 +0100

    add drawing functionality

--- socketsBase.js
+++ socketsBase.js
@@ -1,6 +1,9 @@
 module.exports = function (io){
     io.on('connection', function(socket){
         console.log("a user connected");
+        socket.on('draw', function(p){
+             io.emit('draw',p);
+        });
         socket.on('disconnect', function(){
             console.log("disconnected :(");
         });
