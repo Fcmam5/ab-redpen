c2e91279b23c71bb0e4c4ab2ce92625d41229d2e
fcmam5 <dr.fcmam5@gmail.com>
2016-11-26 04:10:37 +0100

    remote drawing

--- socketsBase.js
+++ socketsBase.js
@@ -1,10 +1,11 @@
 module.exports = function (io){
     io.on('connection', function(socket){
         console.log("a user connected");
-        socket.on('draw', function(p){
-
-            console.log("oh :"+p.x);
-             io.emit('draw',p);
+        socket.on('draw', function(p,b){
+             io.emit('draw',p,b);
+        });
+        socket.on('moved', function(p){
+             io.emit('moved',p);
         });
         socket.on('disconnect', function(){
             console.log("disconnected :(");
