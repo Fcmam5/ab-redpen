c3b890a97f7a8a0a712e98a2304d2a125e33284c
fcmam5 <dr.fcmam5@gmail.com>
2016-11-26 21:54:42 +0100

    Add choosing picture functionality

---
+++ public/javascripts/whiteboard.js
@@ -0,0 +1,70 @@
+$(document).ready(function(){
+var socket = io();
+var canvas = $("#red-pen")[0];
+var ctx = canvas.getContext('2d');
+var pos = {x:0, y:0};
+var isDown = false;
+
+canvas.height = $('#the-board').height();
+canvas.width = $('#the-board').width();
+
+var setPosition = function(a,b){
+    pos.x = a;
+    pos.y = b;
+};
+
+var drawing = function(x,y,down){
+    if (down) {
+    ctx.beginPath();
+    ctx.lineWidth = 10;
+    ctx.lineJoin = 'round';
+    ctx.strokeStyle = 'red';
+    ctx.moveTo(pos.x,pos.y);
+    setPosition(x,y);
+    ctx.lineTo(pos.x,pos.y);
+    ctx.closePath();
+    ctx.stroke();
+    }
+    pos.x = x;
+    pos.y = y;
+};
+
+$('#the-board').on('mousedown',function(p){
+    isDown = true;
+    drawing(p.pageX - $(this).offset().left,
+             p.pageY - $(this).offset().top,
+             isDown);
+
+});
+
+$("#the-board").on('mousemove', function(p) {
+        drawing(p.pageX - $(this).offset().left,
+                 p.pageY - $(this).offset().top,
+                 isDown);
+    socket.emit('draw',pos,isDown);
+    $("#le-me").text(pos.x+","+pos.y);
+});//end mousemove event
+
+$("#the-board").on('mouseleave mouseup', function(){
+    isDown = false;
+});
+
+socket.on('draw',function (ps,b) {
+    drawing(ps.x,
+             ps.y,
+             b);
+             $("#le-him").text(ps.x+","+ps.y);
+
+});
+
+//Get the photo from user
+$('#btn').click(function(){
+    var imgUrl = $('#img-link').val();
+    socket.emit('change-bg',imgUrl);
+});
+
+socket.on('change-bg',function(img){
+    console.log(img);
+    $("#the-board").css("background-image","url("+img+")");
+});
+});
--- socketsBase.js
+++ socketsBase.js
@@ -7,6 +7,10 @@ module.exports = function (io){
         socket.on('moved', function(p){
              io.emit('moved',p);
         });
+        socket.on('change-bg', function(img) {
+            io.emit('change-bg',img);
+            console.log("tbe user changed the bg to: "+img);
+        });
         socket.on('disconnect', function(){
             console.log("disconnected :(");
         });
